e4e81791d2ee9a1ba7c8d9f8320cc4a7e431ce93
potensun <potensun@gmail.com>
2023-04-10 14:37:09 +0900

    반복문 연습예제

---
+++ src/ForTest.java
@@ -0,0 +1,19 @@
+
+public class ForTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0;i<5;i++) {
+			System.out.println("안녕!!!");
+		}
+	
+		int j = 0;
+		
+		while(j<5) {
+			System.out.println("안녕!!!");
+			j++;
+		}
+	}
+
+}
