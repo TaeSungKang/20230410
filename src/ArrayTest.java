ade633b4d450dfb9e62b99bfe1b77e5f46e6ac73
JBEDU04 <JBEDU04@DESKTOP-HMF1SKG>
2023-04-10 16:05:50 +0900

    array와 return

---
+++ src/ArrayTest.java
@@ -0,0 +1,36 @@
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[] = new int[10];
+		
+		int b[] = {10,20,30,40,50};//배열의 선언과 동시에 초기화
+		
+		int c[] = {321,321,3,13,1,31,32,13,13,1,31,5,6,4,897,9};
+		
+//		int c;
+//		int d=10;
+		a[5] = 100;
+		int k = b[4];
+
+		for(int i=0;i<5;i++) {
+//			System.out.println(b[i]);
+		}
+		int count=0;
+		
+		for(int j=0;j<c.length;j++) {
+//			System.out.println(c[j]);	
+			count++;
+		}
+		System.out.println(count);
+		
+//		System.out.println(a[5]);
+//		
+		System.out.println(c.length);
+		System.out.println(b);
+
+	}
+
+}
